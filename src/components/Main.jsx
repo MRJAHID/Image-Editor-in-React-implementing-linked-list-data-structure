25824720799f2587ebf5c024a42fbaa79737f927
MRJAHID <mrjahid350@gmail.com>
2022-08-03 11:31:58 +0600

    sass integrate

---
+++ src/components/Main.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+import "./style/main.scss";
+
+const Main = () => {
+  return (
+    <div className="image_editor">
+      <div className="card">
+        <div className="card_header">
+          <h2>----------- Image Editor ----------</h2>
+        </div>
+        <div className="card_body">
+          <div className="sidebar"></div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Main;
